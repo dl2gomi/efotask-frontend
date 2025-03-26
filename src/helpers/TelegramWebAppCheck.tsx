9025147e169df1cb04f00c6a3ce740cdfc8c048b
Denis <dl2gomi@gmail.com>
2025-03-26 10:54:45 -0500

    fixed minor error about Telegram property of window

--- src/helpers/TelegramWebAppCheck.tsx
+++ src/helpers/TelegramWebAppCheck.tsx
@@ -3,14 +3,14 @@ import { useEffect } from 'react';
 const TelegramWebAppCheck = () => {
   useEffect(() => {
     // Check if the app is running inside Telegram Web App
-    if (!window.Telegram || !window.Telegram.WebApp) {
+    if (!(window as any).Telegram || !(window as any).Telegram?.WebApp) {
       // If not in Telegram, display a message or redirect the user
       alert('This app can only be used inside Telegram');
       // Optionally, redirect the user (e.g., to a specific page or homepage)
       window.location.href = 'https://t.me/efotask_bot'; // Redirect to your bot or a page
     } else {
       // Initialize Telegram Web App
-      const webApp = window.Telegram.WebApp;
+      const webApp = (window as any).Telegram.WebApp;
       webApp.ready();
     }
   }, []);
--- src/hooks/useTelegramUser.ts
+++ src/hooks/useTelegramUser.ts
@@ -4,8 +4,8 @@ const useTelegramUser = () => {
   const [userId, setUserId] = useState(null);
 
   useEffect(() => {
-    if (window.Telegram && window.Telegram.WebApp) {
-      const webApp = window.Telegram.WebApp;
+    if ((window as any).Telegram && (window as any).Telegram.WebApp) {
+      const webApp = (window as any).Telegram.WebApp;
       webApp.ready();
 
       const user = webApp.initDataUnsafe; // User data from Telegram Web App
